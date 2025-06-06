93aa163877ea14a25ae09f5830172ad18a204f67
Kunzeu <kunjohn24@gmail.com>
2025-06-06 16:19:44 -0500

    Primera versión de producción

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
