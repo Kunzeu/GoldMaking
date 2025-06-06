94f425e9aaa0f093a67c2b435fe32262c71a4097
Kunzeu <kunjohn24@gmail.com>
2025-06-06 16:34:33 -0500

    Primera versión de producción

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "SQLALCHEMY_DATABASE_URI"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "tu_clave_secreta_aqui"
