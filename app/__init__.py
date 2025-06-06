61fa0d971d46b332b9e1f8680c2cf0652c56099f
Kunzeu <kunjohn24@gmail.com>
2025-06-06 18:03:50 -0500

    Primera versión de producción

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from werkzeug.security import generate_password_hash
+import os
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
+    load_dotenv()  # Carga variables del .env
+    
     app = Flask(__name__)
-    app.config.from_object('config.Config')  # tu configuración
+    app.config.from_object('config.Config')
 
     db.init_app(app)
     login_manager.init_app(app)
-
-    login_manager.login_view = 'auth.login'  # nombre de la ruta login
+    login_manager.login_view = 'auth.login'
 
     from .routes import main_bp
     from .auth import auth_bp
@@ -22,29 +24,30 @@ def create_app():
     app.register_blueprint(auth_bp)
 
     with app.app_context():
-        from .models import User  # importa dentro del contexto para evitar ciclos
-        db.create_all()  # crea tablas si no existen
-        create_default_user()
+        from .models import User
+        from werkzeug.security import generate_password_hash
+
+        username = os.getenv('ADMIN_USER')
+        password = os.getenv('ADMIN_PASS')
+        role = os.getenv('ADMIN_ROLE')
+
+        admin = User.query.filter_by(username=username).first()
+        if not admin:
+            admin = User(
+                username=username,
+                password=generate_password_hash(password),
+                role=role
+            )
+            db.session.add(admin)
+            db.session.commit()
+            print(f"✔ Usuario {username} creado")
+        else:
+            print(f"ℹ Usuario {username} ya existe")
 
     return app
 
+from .models import User
+
 @login_manager.user_loader
 def load_user(user_id):
-    from .models import User
     return User.query.get(int(user_id))
-
-
-def create_default_user():
-    from .models import User
-    admin = User.query.filter_by(username='admin').first()
-    if not admin:
-        admin = User(
-            username='admin',
-            password=generate_password_hash('admin123'),
-            role='admin'
-        )
-        db.session.add(admin)
-        db.session.commit()
-        print("✔ Usuario admin creado")
-    else:
-        print("ℹ Usuario admin ya existe")
