3dc6b6b15ce29fe0a4430cd55a35b59674813677
Kunzeu <kunjohn24@gmail.com>
2025-06-06 17:51:24 -0500

    Primera versión de producción

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -19,13 +20,31 @@ def create_app():
 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
-    
 
+    with app.app_context():
+        from .models import User  # importa dentro del contexto para evitar ciclos
+        db.create_all()  # crea tablas si no existen
+        create_default_user()
 
     return app
 
-from .models import User  # importa el modelo User
-
 @login_manager.user_loader
 def load_user(user_id):
+    from .models import User
     return User.query.get(int(user_id))
+
+
+def create_default_user():
+    from .models import User
+    admin = User.query.filter_by(username='admin').first()
+    if not admin:
+        admin = User(
+            username='admin',
+            password=generate_password_hash('admin123'),
+            role='admin'
+        )
+        db.session.add(admin)
+        db.session.commit()
+        print("✔ Usuario admin creado")
+    else:
+        print("ℹ Usuario admin ya existe")
